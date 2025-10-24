e0c94b67ff74e6317aab6af8f8f70178bc9ef42c
kdh <kdh@dev.com>
2025-10-24 14:05:38 +0900

    Fixed: 구동 시 에러 수정 - r2dbc 테스트 제외

--- src/test/kotlin/com/demo/sample/SampleApplicationTests.kt
+++ src/test/kotlin/com/demo/sample/SampleApplicationTests.kt
@@ -1,9 +1,12 @@
 package com.demo.sample
 
 import org.junit.jupiter.api.Test
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration
+import org.springframework.boot.autoconfigure.r2dbc.R2dbcAutoConfiguration
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
+@ImportAutoConfiguration(exclude = [R2dbcAutoConfiguration::class])
 class SampleApplicationTests {
 
 	@Test
